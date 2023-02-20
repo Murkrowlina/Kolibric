293469ce6c26a4f471bf86f3d0d2d28adae26e9c
Murkrowlina <nikovuliclina@gmail.com>
2023-02-20 16:28:02 +0100

    Pixel Art

--- main.js
+++ main.js
@@ -42,3 +42,49 @@ function loadBallGame(){
     document.getElementsByClassName('ball')[izabrana_loptica].style.backgroundColor = '#' + random_hex_2;
     document.getElementsByClassName('ball')[izabrana_loptica].addEventListener('click', loadBallGame);
 }
+
+// PIXEL ART
+let pixel_canvas = document.getElementById('pixel-art-container');
+let input_size = document.getElementById('size-input');
+let new_size = input_size.value;
+let input_color = document.getElementById('color-input');
+let input_draw = false;
+
+function createPixels(size){
+    pixel_canvas.style.setProperty('--size', size)
+
+    for(let i = 0; i < size * size; i++){
+        let div_el = document.createElement('div');
+        div_el.classList.add('pixel');
+        
+        div_el.addEventListener('mouseover', function(){
+            if(!input_draw) return
+            div_el.style.backgroundColor = input_color.value;
+        });
+        div_el.addEventListener('mousedown', function(){
+            div_el.style.backgroundColor = input_color.value;
+        });
+
+        pixel_canvas.appendChild(div_el);
+    }
+}
+
+window.addEventListener('mousedown', function(){
+    input_draw = true;
+});
+
+window.addEventListener('mouseup', function(){
+    input_draw = false;
+});
+
+document.getElementById('reset-pixels').addEventListener('click', function(){
+    for(let i = 0; i < document.querySelectorAll('.pixel').length; i++){
+        document.getElementsByClassName('pixel')[i].style.backgroundColor = 'white';
+    }
+});
+
+document.getElementById('new-canvas').addEventListener('click', function(){
+    pixel_canvas.innerHTML = '';
+    new_size = input_size.value;
+    createPixels(new_size);
+})
